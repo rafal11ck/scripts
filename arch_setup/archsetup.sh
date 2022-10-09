519d02b4013581f5851a9a08432a9d0b006c7af8
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2022-10-09 18:52:50 +0200

    fix

---
+++ arch_setup/archsetup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#Varables
+fonts="noto-fonts"
+clitools="tmux git neovim"
+audio="pipewire pipewire-jack"
+filemanagment="pcmanfm "
+username="rafal"
+
+#make sure to run as root
+if [ "$(whoami)" = "root" ]
+then
+printf %s "Not root! run this as root\n"
+exit 1
+fi
+
+# replace /bin/sh with dash
+pacman -S dash
+rm /bin/sh
+ln -s "$(which dash)" "/bin/sh"
+
+#replace sudo with does
+pacman -R sudo
+pacman -S doas
+echo "permit persist :wheel as root" >> /etc/doas.conf
+ln -s "$(which doas)" /bin/sudo;
+
+# add user
+useradd --create-home --user-group -groups wheel "$username"
+echo "Set password for $username"
+passwd "$username"
+#+end_src
+
+#setup user
+su -c "mkdir ~/Desktop ~/Downloads ~/Documents" "$username"
+pacman -S "ripgrep emacs fd git"
+su -c "git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d ~/.emacs.d/bin/doom install" "$username"
