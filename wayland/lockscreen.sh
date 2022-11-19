22355f5cd587cdc263293dfe03a4ada82d05f4c0
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2022-11-19 21:38:22 +0100

    scripts

--- wayland/lockscreen.sh
+++ wayland/lockscreen.sh
@@ -2,4 +2,4 @@
 #Runs lockscreen
 #Requires swaylock
 
-swaylock --image "$HOME/Pictures/wallpapers/arch-linux/archlinux-logo-light-no-tm.png" --ring-color 8218c4 --key-hl-color 3e0c60
+swaylock --scaling fit --image "$HOME/Pictures/wallpapers/arch-linux/archlinux-logo-light-no-tm.png" --ring-color 8218c4 --key-hl-color 3e0c60
--- wayland/suspendtimer.sh
+++ wayland/suspendtimer.sh
@@ -3,4 +3,6 @@
 
 swayidle -w \
     timeout 600 'systemctl suspend' \
-    before-sleep "$HOME/.scripts/wayland/lockscreen.sh"
+    before-sleep ""$HOME"/.scripts/wayland/lockscreen.sh"
+
+# before-sleep 'swaylock --scaling fit --image /home/rafal/Pictures/wallpapers/arch-linux/archlinux-logo-light-no-tm.png --ring-color 8218c4 --key-hl-color 3e0c60'
