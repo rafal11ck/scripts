2cb1bc47e596b68726d9332f026eb1a6d55b9443
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2022-11-25 17:57:01 +0100

    script

--- arch_setup/setupDE.sh
+++ arch_setup/setupDE.sh
@@ -1,8 +1,20 @@
 #install basic stuff
-pacman -S openssh neovim tmux
+doas pacman -Syu --needed openssh neovim tmux fakeroot rust pkgconfig tar dict
 
-#install
-sudo pacman -S --needed base-devel
+#install AUR helper\
+doas sudo pacman -S --needed base-devel fakeroot rust pkgconfig
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+
+paru -Syu --needed noto-fonts oft-ipafont ttf-baekmuk opendesktop-fonts noto-fonts-emoji
+
+paru -Syu --needed pipewire
+
+paru -Syu --needed foot pcmanfm-gtk3
+
+paru -Syu emacs-pgtk-git
+
+paru -Syu --needed git ripgrep fd find
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+~/.emacs.d/bin/doom install
