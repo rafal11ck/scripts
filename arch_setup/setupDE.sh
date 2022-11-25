28bf4f1ddb64f148181799dc3d870ee71bd5bed4
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2022-11-25 18:23:41 +0100

    renaame and tangle

--- arch_setup/setupDE.sh
+++ arch_setup/setupDE.sh
@@ -1,5 +1,5 @@
 #install basic stuff
-doas pacman -Syu --needed openssh neovim tmux fakeroot rust pkgconfig tar dict
+doas pacman -Syu --needed openssh neovim tmux fakeroot rust pkgconfig tar dictd gitui mandb
 
 #install AUR helper\
 doas sudo pacman -S --needed base-devel fakeroot rust pkgconfig
@@ -7,14 +7,15 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -Syu --needed noto-fonts oft-ipafont ttf-baekmuk opendesktop-fonts noto-fonts-emoji
+paru -Syu --needed noto-fonts otf-ipafont ttf-baekmuk opendesktop-fonts noto-fonts-emoji ttf-iosevka-nerd
 
 paru -Syu --needed pipewire
 
 paru -Syu --needed foot pcmanfm-gtk3
 
-paru -Syu emacs-pgtk-git
+paru -Syu --needed autoconf makeinfo
+paru -Syu --needed emacs-pgtk-git
 
-paru -Syu --needed git ripgrep fd find
+paru -Syu --needed git ripgrep fd find texlive-full
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
