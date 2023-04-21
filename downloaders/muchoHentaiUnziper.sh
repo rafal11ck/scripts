d08b30a0a9014b1403386981b3bff97342097a20
Rafał <example@invalid.com>
2023-04-21 16:47:41 +0200

    unpacker

---
+++ downloaders/muchoHentaiUnziper.sh
@@ -0,0 +1,25 @@
+[ $# -eq 0 ] && echo "Specify path to run script at"
+
+echo "Runnning at $(realpath "$1")"
+
+cd "$(realpath "$1")" || exit
+
+toUnpack="$(ls ./*zip)"
+
+echo "Will unpack:"
+echo "$toUnpack"
+
+echo "Exit script to cancel, enter any input to continue."
+read -r
+
+for i in ./*zip; do
+    printf %b "\nNow unpacking:\t $i"
+    printf %b "\n\tUnziping..."
+    unzip "$i" >/dev/null || exit
+    printf "Done"
+    newname="$(basename "$i" .zip).mkv"
+    printf %b "\n\tRenaming as $newname\n"
+    rename "video.mkv" "$newname" ./video.mkv || exit
+    printf %b "\n\tRemoving $i\n"
+    rm "$i" || exit
+done
