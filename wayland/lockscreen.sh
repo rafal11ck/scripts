e285f4459501df56c26c1ebd444d9f654d4734f1
Your Name <you@example.com>
2024-04-30 15:07:31 +0200

    lockscreen

--- wayland/lockscreen.sh
+++ wayland/lockscreen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 #Runs lockscreen
 
-hyprlock -q
+#hyprlock -q
 
 #Requires swaylock
-#swaylock --scaling fit --image "$HOME/Pictures/wallpapers/arch-linux/archlinux-logo-light-no-tm.png" --ring-color 8218c4 --key-hl-color 3e0c60 --color 000000ff
+swaylock --scaling fit --image "$HOME/Pictures/wallpapers/1.png" --ring-color 8218c4 --key-hl-color 3e0c60 --color 000000ff
